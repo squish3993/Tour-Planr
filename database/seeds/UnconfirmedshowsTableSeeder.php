bc5b84affd73d9bc1f5198f7344445561bbaa6c8
squish3993 <jdlewis@wesleyan.edu>
2018-02-20 14:32:02 -0500

    finished unconfirmed show seeder

--- database/seeds/UnconfirmedshowsTableSeeder.php
+++ database/seeds/UnconfirmedshowsTableSeeder.php
@@ -13,17 +13,27 @@ class UnconfirmedshowsTableSeeder extends Seeder
      */
     public function run()
     {
-     $unconfirmedshows = [
-     	['New York', 1, 2018-08-18],
-     	['Philadelphia', 2, 2018-08-20],
-     	['Chicago', 1, 2018-08-24],
-     	['St. Louis', 1, 2018-08-25],
-     	['Washington D.C', 2, 2018-08-28],
-     	['Amherst', 3, 2018-08-8-30],
-     ];
+         $unconfirmedshows = [
+         	['New York', 1, '2018-08-18'],
+         	['Philadelphia', 2, '2018-08-20'],
+         	['Chicago', 1, '2018-08-24'],
+         	['St. Louis', 1, '2018-08-25'],
+         	['Washington D.C', 2, '2018-08-28'],
+         	['Amherst', 3, '2018-08-30'],
+         ];
 
-     $count = count($unconfirmedshows);
+         $count = count($unconfirmedshows);
 
-     foreach ($)   
+         foreach ($unconfirmedshows as $key => $unconfirmedshow) 
+         {
+            Unconfirmedshow::insert([
+                'created_at' => Carbon\Carbon::now()->subDays($count)->toDateTimeString(),
+                'updated_at' => Carbon\Carbon::now()->subDays($count)->toDateTimeString(),
+                'city' => $unconfirmedshow[0],
+                'tier' => $unconfirmedshow[1],
+                'date' => $unconfirmedshow[2]
+            ]);
+            $count--;
+         }  
     }
 }
