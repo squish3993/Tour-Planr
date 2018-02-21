200b7649b7ef34587b8a5ecca6ff31809bbb42d2
squish3993 <jdlewis@wesleyan.edu>
2018-02-21 13:21:48 -0500

    first layouts

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			@yield('title', 'Jakals Tour Planr')
+		</title>
+
+		<meta charset='utf-8'>
+       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+       <link href="/css/app.css" type='text/css' rel='stylesheet'>
+
+	</head>
+
+
+	<body>
+		<header>
+            <div class='bigtitle container text-align:center'>   
+                <h1> Jakals Tour Planr </h1>
+           </div>
+        </header>
+
+        <section id='sidebar' class=' flex-container col-3'>
+        	@yield('sidebar')
+        </section>
+
+
+
+	</body>
+</html>
